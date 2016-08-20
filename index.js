30a0c351621320f1daf01f232bc4bcd810b3a275
Jason <Jason@localhost.localdomain>
2016-08-21 04:55:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: Jiang Guoxi
+* @Date:   2016-08-21 04:26:58
+* @Last Modified by:   Jiang Guoxi
+* @Last Modified time: 2016-08-21 04:28:07
+*/
+
---
+++ schema/article.js
@@ -0,0 +1,7 @@
+/*
+* @Author: Jiang Guoxi
+* @Date:   2016-08-21 04:36:09
+* @Last Modified by:   Jiang Guoxi
+* @Last Modified time: 2016-08-21 04:37:17
+*/
+const mongoose = require('mongoose');
